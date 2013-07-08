54c78a0eee829263d6b7e5c2933d19efd6e1307c
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-07-08 13:30:08 -0400

    Added staging environment
    Also updated the production config based on changes in Rails 4 (some
    config items weren't needed or were renamed).

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,8 @@
 GradeCraft::Application.configure do
-  config.action_controller.default_url_options = { :host => 'gradecraft.com' }
+  config.action_controller.default_url_options = { :host => 'gradecraft.townstage.com' }
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.action_mailer.default_url_options = { :host => 'gradecraft.com' }
+  config.action_mailer.default_url_options = { :host => 'gradecraft.townstage.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
