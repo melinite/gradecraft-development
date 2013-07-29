8bb7caac2c2999e85566aa27216f560ad46dade3
Caitlin Holman <caitlin.holman@gmail.com>
2013-07-29 19:25:10 -0400

    Working challenge creation

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,70 @@
+class ChallengesController < ApplicationController
+
+  before_filter :ensure_staff?, :only=>[:new,:edit,:create,:update,:destroy]
+
+  def index
+    @title = "View All Challenges"
+    @challenges = current_course.challenges
+  end
+
+  def show
+    @challenge = current_course.challenges.find(params[:id])
+    @title = @challenge.name
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @challenge }
+    end
+  end
+
+  def new
+    @challenge = current_course.challenges.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @challenge }
+    end
+  end
+
+  def edit
+    @challenge = current_course.challenges.find(params[:id])
+  end
+
+  def create
+    @challenge = current_course.challenges.create(params[:challenge])
+
+    respond_to do |format|
+      if @challenge.save
+        format.html { redirect_to @challenge, notice: 'challenge was successfully created.' }
+        format.json { render json: @challenge, status: :created, location: @challenge }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @challenge.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @challenge = current_course.challenges.find(params[:id])
+
+    respond_to do |format|
+      if @challenge.update_attributes(params[:challenge])
+        format.html { redirect_to @challenge, notice: 'challenge was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @challenge.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @challenge = current_course.challenges.find(params[:id])
+    @challenge.destroy
+
+    respond_to do |format|
+      format.html { redirect_to challenges_path }
+      format.json { head :ok }
+    end
+  end
+  
+end
---
+++ app/models/challenge.rb
@@ -0,0 +1,23 @@
+class Challenge < ActiveRecord::Base
+  self.table_name = 'assignments'
+  
+  default_scope -> { where(:type => 'Challenge') }
+  
+  attr_accessible :assignment, :assignment_id, :name, :description, :icon,
+    :visible, :created_at, :updated_at, :image_file_name, :occurrence,
+    :badge_set, :category_id, :value, :multiplier, :point_total, :due_date,
+    :submissions_allowed, :release_necessary
+    
+  has_many :tasks, :foreign_key => :assignment_id, :dependent => :destroy
+  belongs_to :course
+  has_many :submissions
+
+  validates_presence_of :course, :name
+  
+  def grade_scope
+    'TeamChallenge'
+  end
+
+  private
+  
+end
