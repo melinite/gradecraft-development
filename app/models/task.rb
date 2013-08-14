d20527190544790920d24360fd17684fba3414c6
Caitlin Holman <caitlin.holman@gmail.com>
2013-08-14 10:32:45 -0400

    Updated page navs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :assignment, :assignment_id, :assignment_type, :title, :description, :due_at
 
-  belongs_to :assignment, :polymorphic => true
+  belongs_to :assignment, :foreign_key => :assignment_id, :polymorphic => true
   belongs_to :course
   has_many :submissions, :dependent => :destroy
 
