690915177b6ad2a8e7bb1a7492c75f6a87eab957
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-08-23 15:12:08 -0400

    Make New Relic app name configurable

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module GradeCraft
   class Application < Rails::Application
     config.time_zone = 'America/Detroit'
     config.autoload_paths += %W(#{Rails.root}/lib)
-    config.assets.paths += %W(#{Rails.root}/vendor/assets/fonts)
     config.assets.precompile += %w(.svg .eot .otf .woff .ttf)
     config.generators do |g|
       g.integration_tool :mini_test
