08b8369e404ce9099e5067d820a81cd81c4afa18
Caitlin Holman <caitlin.holman@gmail.com>
2013-08-24 15:18:33 -0400

    Added conditional displays to student dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824184813) do
+ActiveRecord::Schema.define(version: 20130824190030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -340,6 +340,7 @@ ActiveRecord::Schema.define(version: 20130824184813) do
     t.text     "feedback"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "shared"
   end
 
   create_table "elements", force: true do |t|
