1c2b2d8133b95952f2c550776fec05b35d32bab1
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-09-03 12:29:25 -0400

    Move lti key/secret into env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :lti, :oauth_credentials => { 'Caitlin Holman' => 'berlin' }
+  provider :lti, :oauth_credentials => { ENV['LTI_CONSUMER_KEY'] => ENV['LTI_CONSUMER_SECRET'] }
   provider :kerberos, uid_field: :username, fields: [ :username, :password ]
 end
