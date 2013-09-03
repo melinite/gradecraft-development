bcc09533350a65b2af5e2b2029ca6a1ab958ebbc
Caitlin Holman <caitlin.holman@gmail.com>
2013-09-02 22:53:00 -0400

    Social display of badges works

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -7,13 +7,13 @@ class InfoController < ApplicationController
     if current_user.is_staff?
       @teams = current_course.teams.includes(:earned_badges)
       @users = current_course.users
-      @students = @users.students
       @top_ten_students = @students.order_by_high_score.limit(10)
       @bottom_ten_students = @students.order_by_low_score.limit(10)
       @submissions = current_course.submissions
     end
     @badges = current_course.badges.includes(:tasks)
     @user = current_user
+    @students = current_course.users.students
     @assignments = current_course.assignments.includes(:assignment_type)
     @assignment_types = current_course.assignment_types.includes(:assignments)
   end
