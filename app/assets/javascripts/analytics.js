dc8355171b2e9ceb1fbb0203d8862f6a95f0dcb6
Steve Schwartz <steve@alfajango.com>
2013-09-04 03:13:47 -0400

    Extracted front-end chart info to data attributes of div element.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -11,71 +11,81 @@ var getKey = function(o, k) {
   return o;
 };
 
-var loadAssignmentEvents = function() {
-  $.getJSON('/analytics/assignment_events.json', function(response) {
-    var range = response.range,
-        series = [],
-        dateStart = range[0]*1000,
-        dateInterval = (range[1] - range[0])*1000,
-        keys = response.keys;
+var loadAnalytics = function() {
+  $('[data-chart]').each( function() {
+    var $this = $(this),
+        chartType = $this.data('chart');
 
-    for (var i = 0, ilen = response.data.length; i < ilen; i++) {
-      var record = response.data[i],
-          s = {
-            name: record.name,
-            data: [],
-            pointInterval: dateInterval,
-            pointStart: dateStart
-          };
+    $.getJSON($this.data('url'), function(response) {
 
-      for (var t = 0, tlen = range.length; t < tlen; t++) {
-        var x = range[t],
-            key = getKey(record, response.key),
-            y = key[x] ? key[x] : 0;
-        s.data.push( y );
-      }
+      if (chartType === 'timeseries') {
+        var range = response.range,
+            series = [],
+            dateStart = range[0]*1000,
+            dateInterval = (range[1] - range[0])*1000,
+            keys = response.keys;
 
-      series.push(s);
-    }
+        for (var i = 0, ilen = response.data.length; i < ilen; i++) {
+          var record = response.data[i],
+              s = {
+                name: record.name,
+                data: [],
+                pointInterval: dateInterval,
+                pointStart: dateStart
+              };
 
-    $('#events-chart').highcharts({
-      title: {
-        text: 'Assignment Events',
-                  x: -20 //center
-      },
-      subtitle: {
-        text: 'per minute',
-                x: -20
-      },
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: { // don't display the dummy year
-          minute: '%H:%M'
+          for (var t = 0, tlen = range.length; t < tlen; t++) {
+            var x = range[t],
+                key = getKey(record, response.key),
+                y = key[x] ? key[x] : 0;
+            s.data.push( y );
+          }
+
+          series.push(s);
         }
-      },
-      yAxis: {
-        title: {
-          text: 'Number of events'
-        },
-        plotLines: [{
-          value: 0,
-              width: 1,
-              color: '#808080'
-        }]
-      },
-      legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-        borderWidth: 0
-      },
-      series: series
+
+        $this.highcharts({
+          title: {
+            text: $this.data('title'),
+                      x: -20 //center
+          },
+          subtitle: {
+            text: $this.data('subtitle'),
+                    x: -20
+          },
+          xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+              minute: '%H:%M'
+            }
+          },
+          yAxis: {
+            min: 0,
+            title: {
+              text: $this.data('y-axis-label')
+            },
+            plotLines: [{
+              value: 0,
+                  width: 1,
+                  color: '#808080'
+            }]
+          },
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+          },
+          series: series
+        });
+      };
     });
+
   });
 };
 
 $(document).delegate('#refresh', 'click', function(e) {
-  loadAssignmentEvents();
+  loadAnalytics();
   e.preventDefault();
 });
 
@@ -85,5 +95,5 @@ $(function () {
       useUTC: false
     }
   });
-  loadAssignmentEvents();
+  loadAnalytics();
 });
