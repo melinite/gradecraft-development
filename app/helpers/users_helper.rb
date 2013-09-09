afbf3bb0878b4bf65927947acf0277598d6c8bdf
Caitlin Holman <caitlin.holman@gmail.com>
2013-09-09 14:15:57 -0400

    Responsive gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
-  def gravatar_for(user, options = { :size => 50 })
+  def gravatar_for(user, options = { })
     gravatar_image_tag(user.email.downcase, :alt => h(user.name),
                                             :class => 'img-rounded',
                                             :gravatar => options)
-                                            
+
   end
 end
