e7385aa1b171ad0052bc964937da23ba518a814a
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-09-10 23:53:55 -0400

    Set up sidekiq / redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
 Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV['REDIS_URL'], :namespace => 'gradecraft_production' }
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => ENV['REDIS_URL'], :namespace => 'gradecraft_production' }
 end
