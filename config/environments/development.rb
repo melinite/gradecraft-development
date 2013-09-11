cee05e766c1d025f52b38501110e1a31e07d7f29
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-09-11 13:49:04 -0400

    Switch back to cookie storage for sessions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 GradeCraft::Application.configure do
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.action_dispatch.best_standards_support = :builtin
   config.action_mailer.default_url_options = { :host => 'gradecraft:3000' }
   config.action_mailer.delivery_method = :test
@@ -9,6 +9,7 @@ GradeCraft::Application.configure do
   config.assets.compress = false
   config.assets.debug = true
   config.cache_classes = false
+  config.cache_store = :null_store
   config.consider_all_requests_local = true
   config.eager_load = false
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-#GradeCraft::Application.config.session_store :cookie_store, key: '_GradeCraft_session'
-#adding Dalli caching
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 60.minutes
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# GradeCraft::Application.config.session_store :active_record_store
+GradeCraft::Application.config.session_store :cookie_store, key: '_gradeCraft_session', :expire_after => 60.minutes
+# Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 60.minutes
