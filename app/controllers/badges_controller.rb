17644851ab437723a956728a2e2f8031cb3fb66f
mgobirch <birch1bs@gmail.com>
2013-09-13 16:01:05 -0400

    Updated Badges scope
    Second commit, updated badges_controller with new scope and filter for staff

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,6 @@
 class BadgesController < ApplicationController
 
-  before_filter :ensure_staff?, :only=>[:new,:edit,:create,:update,:destroy]
+  before_filter :ensure_staff?, :except =>[:show]
 
   def index
     @title = "View All #{term_for :badges}"
