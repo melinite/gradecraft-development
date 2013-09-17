fab9a90c452cede44ca6306223fff9820a8a4e28
mgobirch <birch1bs@gmail.com>
2013-09-17 18:54:44 -0400

    Criteria levels controller done
    added scope changes to criteria levels controller

--- app/controllers/criteria_levels_controller.rb
+++ app/controllers/criteria_levels_controller.rb
@@ -1,37 +1,37 @@
 class CriteriaLevelsController < ApplicationController
   def index
-    @criteria_levels = CriteriaLevel.all
+    @criteria_levels = current_course.criteria_level.all
     respond_with(@criteria_levels)
   end
 
   def show
-    @criteria_level = CriteriaLevel.find(params[:id])
+    @criteria_level = current_course.criteria_level.find(params[:id])
     respond_with(@criteria_level)
   end
 
   def new
-    @criteria_level = CriteriaLevel.new
+    @criteria_level = current_course.criteria_level.new
     respond_with(@criteria_level)
   end
 
   def edit
-    @criteria_level = CriteriaLevel.find(params[:id])
+    @criteria_level = current_course.criteria_level.find(params[:id])
   end
 
   def create
-    @criteria_level = CriteriaLevel.new(params[:criteria_level])
+    @criteria_level = current_course.criteria_level.new(params[:criteria_level])
     @criteria_level.save
     respond_with(@criteria_level)
   end
 
   def update
-    @criteria_level = CriteriaLevel.find(params[:id])
+    @criteria_level = current_course.criteria_level.find(params[:id])
     @criteria_level.update_attributes(params[:criteria_level])
     respond_with(@criteria_level)
   end
 
   def destroy
-    @criteria_level = CriteriaLevel.find(params[:id])
+    @criteria_level = current_course.criteria_level.find(params[:id])
     @criteria_level.destroy
     respond_with(@criteria_level)
   end
