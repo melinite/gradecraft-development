f49f83e38ef5d978c5161047013e0441fcee085a
Demarius Chrite <chrite.demarius@gmail.com>
2013-09-17 17:07:23 +0000

    Remove `set_student`.

--- app/controllers/assignment_type_weights_controller.rb
+++ app/controllers/assignment_type_weights_controller.rb
@@ -7,11 +7,11 @@ class AssignmentTypeWeightsController < ApplicationController
       @badges = current_course.badges
       @assignments = current_course.assignments
     end
-    respond_with @form = AssignmentTypeWeightForm.new(@student, current_course)
+    respond_with @form = AssignmentTypeWeightForm.new(current_student, current_course)
   end
 
   def mass_update
-    @form = AssignmentTypeWeightForm.new(@student, current_course)
+    @form = AssignmentTypeWeightForm.new(current_student, current_course)
     @form.update_attributes(student_params)
     if @form.save
       redirect_to dashboard_path
@@ -29,11 +29,6 @@ class AssignmentTypeWeightsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:assignment_type_weights_attributes => [:assignment_type_id, :weight])
-  end
-
-  def set_student
-      @student = current_student
-    end
   end
 
   def interpolation_options
