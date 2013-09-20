91b9c654dccd60f04f61b762dce3fdd07addd0fe
Demarius Chrite <chrite.demarius@gmail.com>
2013-09-20 00:42:02 +0000

    Show chart in /charts/team_grade.
    Chart appears blank due to what appears to be a styling issue.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -7,4 +7,8 @@ class ChartsController < ApplicationController
     respond_with @assignments = current_course.assignments.order('name ASC').select {|a| a.grades.released.length > 1}
   end
 
+  def team_grade
+    #respond_with @assignments = current_course.assignments.order('name ASC').select {|a| a.grades.released.length > 1}
+  end
+
 end
