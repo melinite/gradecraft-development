c99c11bc589e46b44e3221e26bddd1124370083f
Caitlin Holman <caitlin.holman@gmail.com>
2013-09-24 21:10:43 -0400

    commented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -90,6 +90,7 @@ $(document).ready(function(){
 	$('#navbar').affix();
 
   // Temporarily commented out to revive dashboard charts & predictor
+/*
 
   $('.slider').each(function(i,slider) {
     $slider = $(slider);
@@ -130,8 +131,8 @@ $(document).ready(function(){
       }
     });
   });
+*/
 
-/*
     $('.slider').each(function(i,slider) {
     $slider = $(slider)
     $slider.slider({
@@ -141,7 +142,6 @@ $(document).ready(function(){
       $(slider).prev("div.assignment > span").html(ui.value)
     });
   });
-*/
 
 
 
