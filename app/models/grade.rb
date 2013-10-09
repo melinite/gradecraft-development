54d01ee6cd353befa81ca4bc1c730a615f190c00
Caitlin Holman <caitlin.holman@gmail.com>
2013-10-08 22:41:42 -0400

    show only graded assignment scores

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -45,6 +45,7 @@ class Grade < ActiveRecord::Base
 
   scope :completion, -> { where(order: "assignments.due_at ASC", :joins => :assignment) }
   scope :released, -> { joins(:assignment).where('status = ? OR NOT assignments.release_necessary', 'Released') }
+  scope :graded, -> { where('status = ?', 'Graded')}
 
   validates_numericality_of :raw_score, integer_only: true
 
