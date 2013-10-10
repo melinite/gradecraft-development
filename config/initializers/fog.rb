83734f89b6c2d32eb9405e8dc37362d5c07765c2
Demarius Chrite <chrite.demarius@gmail.com>
2013-10-10 16:03:09 +0000

    Fix S3 access.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['Key'],
-    :aws_secret_access_key  => ENV['Secret']
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory = "gradecraft.#{Rails.env}"
-  config.fog_public = :false # Not sure
+  config.fog_public = false
 end
