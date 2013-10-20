d8dd59ebb1e33130225309f99cc598470b7694cc
Caitlin Holman <caitlin.holman@gmail.com>
2013-10-20 10:46:43 -0400

    Adding instructor ability to set multipliers

--- app/controllers/assignment_type_weights_controller.rb
+++ app/controllers/assignment_type_weights_controller.rb
@@ -1,29 +1,43 @@
 class AssignmentTypeWeightsController < ApplicationController
 
   def mass_edit
+    @assignment_types = current_course.assignment_types
     if current_user.is_student?
       @user = current_user
-      @assignment_types = current_course.assignment_types
+      respond_with @form = AssignmentTypeWeightForm.new(current_student, current_course)
+    elsif current_user.is_staff?
+      @student = current_course.students.find(params[:student_id])
+      respond_with @form = AssignmentTypeWeightForm.new(@student, current_course)
     end
-    respond_with @form = AssignmentTypeWeightForm.new(current_student, current_course)
   end
 
   def mass_update
-    @form = AssignmentTypeWeightForm.new(current_student, current_course)
+    if current_user.is_student?
+      @form = AssignmentTypeWeightForm.new(current_student, current_course)
+    elsif current_user.is_staff?
+      @form = AssignmentTypeWeightForm.new(@student, current_course)
+    end
     @form.update_attributes(student_params)
     if @form.save
-      redirect_to dashboard_path
+      if current_user.is_student?
+        redirect_to dashboard_path
+      else
+        redirect_to choices_path
+      end
     else
       if current_user.is_student?
         @user = current_user
+      elsif current_user.is_staff?
+        @student = current_course.students.find(params[:student_id])
       end
-      respond_with @form, action: :mass_edit
+      render :mass_edit
     end
   end
 
   private
 
   def student_params
+    debugger
     params.require(:student).permit(:assignment_type_weights_attributes => [:assignment_type_id, :weight])
   end
 
