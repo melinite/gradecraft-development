f41acc71b780366cdd8bed87aa1e6092a1012a07
Caitlin Holman <caitlin.holman@gmail.com>
2013-10-21 16:40:48 -0400

    Updates

--- app/controllers/assignment_type_weights_controller.rb
+++ app/controllers/assignment_type_weights_controller.rb
@@ -2,34 +2,15 @@ class AssignmentTypeWeightsController < ApplicationController
 
   def mass_edit
     @assignment_types = current_course.assignment_types
-    if current_user.is_student?
-      @user = current_user
-      respond_with @form = AssignmentTypeWeightForm.new(current_student, current_course)
-    elsif current_user.is_staff?
-      @student = current_course.students.find(params[:student_id])
-      respond_with @form = AssignmentTypeWeightForm.new(@student, current_course)
-    end
+    respond_with @form = AssignmentTypeWeightForm.new(current_student, current_course)
   end
 
   def mass_update
-    if current_user.is_student?
-      @form = AssignmentTypeWeightForm.new(current_student, current_course)
-    elsif current_user.is_staff?
-      @form = AssignmentTypeWeightForm.new(@student, current_course)
-    end
+    @form = AssignmentTypeWeightForm.new(current_student, current_course)
+
     @form.update_attributes(student_params)
-    if @form.save
-      if current_user.is_student?
-        redirect_to dashboard_path
-      else
-        redirect_to choices_path
-      end
-    else
-      if current_user.is_student?
-        @user = current_user
-      elsif current_user.is_staff?
-        @student = current_course.students.find(params[:student_id])
-      end
+    @form.save
+    if !form.save
       render :mass_edit
     end
   end
@@ -37,7 +18,6 @@ class AssignmentTypeWeightsController < ApplicationController
   private
 
   def student_params
-    debugger
     params.require(:student).permit(:assignment_type_weights_attributes => [:assignment_type_id, :weight])
   end
 
