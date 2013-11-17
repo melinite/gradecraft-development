f5e045fd32beca62f451ae23a1cde90e12b8a8de
Caitlin Holman <caitlin.holman@gmail.com>
2013-11-17 16:37:11 -0500

    Don't show gsis in team list

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -79,19 +79,6 @@ class StudentsController < ApplicationController
     }
   end
 
-  # TODO: Outgoing? AG
-#   def scores_for_current_course
-#      scores = current_course.grades.released.group(:student_id).order('SUM(score)')
-#      id = params[:user_id] || current_user.id
-#      user_score = current_course.grades.released.group(:student_id)
-#                                                 .where(student_id: id).pluck('SUM(score)')
-#      scores = scores.pluck('SUM(score)')
-#      render :json => {
-#       :scores => scores,
-#       :user_score => user_score
-#      }
-#   end
-
   def scores_for_single_assignment
     scores = current_course.grades.released
                                   .joins('join course_memberships on grades.student_id = course_memberships.user_id')
