ecadb7158169feada3e11b9dc08ff88568b4ddcd
Demarius Chrite <chrite.demarius@gmail.com>
2013-11-19 22:03:27 +0000

    Fix filenames.

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,8 +1,7 @@
 module UploadsHelper
   def url_scheme(model_name)
     course = current_course.name + @assignment.name
-    course.gsub! ' ', '.'
-    course.gsub! '..', '.'
-    return "uploads/#{model_name}/#{course}/#{Time.new.to_s}/${filename}"
+    course.gsub! '/[^0-9a-z]/i', '.'
+    return "uploads/#{model_name}/#{course}/#{Time.new.strftime('%Y.%m.%d.%H.%M.%S')}/${filename}"
   end
 end
