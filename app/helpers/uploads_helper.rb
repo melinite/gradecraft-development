35e340eeff0cb6dc23d9b85f504f913a56f9fc71
Demarius Chrite <chrite.demarius@gmail.com>
2013-11-20 17:11:12 +0000

    Create browsable path.
    Upload to S3 with a browsable path that includes the course name, the
    assignment name, the upload date, and the file name. The course name has
    an id appended to it, as does the assignment name, in order to
    distinguish between courses with the name name.

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,7 +1,9 @@
 module UploadsHelper
-  def url_scheme(model_name)
-    course = current_course.name + @assignment.name
-    course.gsub! '/[^0-9a-z]/i', '.'
-    return "uploads/#{model_name}/#{course}/#{Time.new.strftime('%Y.%m.%d.%H.%M.%S')}/${filename}"
+  def submission_url
+    course = "#{current_course.name}.#{current_course.id}"
+    course.gsub!(/[^0-9a-z]/i, '.').gsub!(/\.+/, '.')
+    assignment = "#{@assignment.name}.#{@assignment.id}"
+    assignment.gsub!(/[^0-9a-z]/i, '.').gsub!(/\.+/, '.')
+    return "uploads/submission_files/#{course}/#{assignment}/#{Time.new.strftime('%Y.%m.%d.%H.%M.%S')}/${filename}"
   end
 end
