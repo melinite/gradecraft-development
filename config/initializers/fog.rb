672dee137cf1e505adde66d35f7b317c9bc5dd0c
Demarius Chrite <chrite.demarius@gmail.com>
2013-12-04 22:09:29 +0000

    Make badge icons public.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory = "gradecraft-#{Rails.env}"
-  config.fog_public = false
+  config.fog_public = true
 end
