6857fa7dcba040da06988657b8123b214ff68722
Demarius Chrite <chrite.demarius@gmail.com>
2014-01-06 04:45:31 +0000

    Fix upload deletion and redirection.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,11 @@
 class UploadsController < ApplicationController
   def remove
-    puts params
-    upload = params[:model].classify.constantize.find params.values.first
-    puts upload
+    upload = params[:model].classify.constantize.find params[:upload_id]
     s3 = AWS::S3.new
     bucket = s3.buckets["gradecraft-#{Rails.env}"]
     bucket.objects[CGI::unescape(upload.filepath)].delete
     upload.delete
 
-    redirect_to :controller => params[:redirect][:controller], :action => "show", :id => params[:redirect][:id]
+    redirect_to :back
   end
 end
