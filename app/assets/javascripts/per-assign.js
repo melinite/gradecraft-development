8ec95c01a4964df9b6e3bda2d539781543e974c6
Caitlin Holman <caitlin.holman@gmail.com>
2014-01-20 22:19:39 -0500

    Fixed boxplot display

--- app/assets/javascripts/per-assign.js
+++ app/assets/javascripts/per-assign.js
@@ -6,7 +6,7 @@ $(function () {
       var id = $('.grades_per_assign')[index].getAttribute('data-id');
       $.getJSON('/students/scores_for_single_assignment', { id: id }, function (data) {
         div.sparkline(data.scores, {type: 'box', boxFillColor: '#eee', lineColor: '#333', boxLineColor: '#333', whiskerColor: '#333', outlierLineColor: '#333',
-outlierFillColor: '#F4A425', spotRadius: '10', medianColor: '#0D9AFF', height: '35' } );
+outlierFillColor: '#F4A425', spotRadius: '10', medianColor: '#0D9AFF', height: '50', width: '300' } );
       })
     })
   }
--- app/assets/javascripts/submission_count.js
+++ app/assets/javascripts/submission_count.js
@@ -8,7 +8,7 @@ $(function () {
       var id = $('.levels_per_assign')[index].getAttribute('data-id');
       $.getJSON('/students/scores_for_single_assignment', { id: id }, function (data) {
         div.sparkline(data.scores, {type: 'box', boxFillColor: '#eee', lineColor: '#333', boxLineColor: '#333', whiskerColor: '#333', outlierLineColor: '#333',
-outlierFillColor: '#F4A425', spotRadius: '10', medianColor: '#0D9AFF', height: '35' } );
+outlierFillColor: '#F4A425', spotRadius: '10', medianColor: '#0D9AFF', height: '35', width: '300' } );
       })
     })
   }
