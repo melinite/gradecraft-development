b4fe56fc142df7d3316597f33b72f938058d476f
Steve Schwartz <steve@alfajango.com>
2014-03-10 14:00:39 -0400

    AJ-1720 Added original event timestamps to analytics events.

--- app/controllers/analytics_events_controller.rb
+++ app/controllers/analytics_events_controller.rb
@@ -9,6 +9,7 @@ class AnalyticsEventsController < ApplicationController
                                 assignment_id: params[:assignment].to_i,
                                 score: params[:score].to_i,
                                 possible: params[:possible].to_i,
+                                created_at: Time.now
                              )
     render :nothing => true, :status => :ok
   end
@@ -18,7 +19,8 @@ class AnalyticsEventsController < ApplicationController
                               course_id: current_course.id,
                               user_id: current_user.id,
                               user_role: current_user.role,
-                              page: "#{params[:url]}#{params[:tab]}"
+                              page: "#{params[:url]}#{params[:tab]}",
+                              created_at: Time.now
                              )
     render :nothing => true, :status => :ok
   end
